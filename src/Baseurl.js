261f864442836ea20250b9efd1887755cf55b81d
Libin Luvis <libinluvis10@gmail.com>
2022-10-15 20:49:35 +0530

    the port changed

--- src/Baseurl.js
+++ src/Baseurl.js
@@ -1,2 +1,2 @@
-// export const baseURL="http://20.1.218.186/"
-export const baseURL="http://localhost:5000/"
+export const baseURL="http://20.1.218.186/"
+// export const baseURL="http://localhost:5000/"
