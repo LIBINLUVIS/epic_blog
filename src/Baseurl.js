b5f6d5d903f5a9502830f04c321b61902ff6bfdd
LIBINLUVIS <libinluvis10@gmail.com>
2022-12-04 23:45:55 +0530

    Update Baseurl.js

--- src/Baseurl.js
+++ src/Baseurl.js
@@ -2,5 +2,5 @@
  * Base url end point.
  * @type {string}
  */
-export const baseURL="http://20.14.224.174/"
+export const baseURL="https://grumpy-erin-hosiery.cyclic.app/"
 // export const baseURL="http://localhost"
